844c31840c7aea4b4981204c961d9c566497bbd1
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-09-25 18:04:14 +0700

    fix department convert case

--- database/seeders/DefaultSeeder.php
+++ database/seeders/DefaultSeeder.php
@@ -28,71 +28,71 @@ class DefaultSeeder extends Seeder
         Faculty::create(['name' => 'Sekolah Bisnis']);
         Faculty::create(['name' => 'Sekolah Vokasi']);
 
-        Department::create(['name' => 'MANAJEMEN SUMBERDAYA LAHAN', 'faculty_id' => 1]);
-        Department::create(['name' => 'AGRONOMI DAN HORTIKULTURA', 'faculty_id' => 1]);
-        Department::create(['name' => 'PROTEKSI TANAMAN', 'faculty_id' => 1]);
-        Department::create(['name' => 'ARSITEKTUR LANSKAP', 'faculty_id' => 1]);
+        Department::create(['name' => 'Manajemen Sumberdaya Lahan', 'faculty_id' => 1]);
+        Department::create(['name' => 'Agronomi Dan Hortikultura', 'faculty_id' => 1]);
+        Department::create(['name' => 'Proteksi Tanaman', 'faculty_id' => 1]);
+        Department::create(['name' => 'Arsitektur Lanskap', 'faculty_id' => 1]);
 
-        Department::create(['name' => 'KEDOKTERAN HEWAN', 'faculty_id' => 2]);
+        Department::create(['name' => 'Kedokteran Hewan', 'faculty_id' => 2]);
 
-        Department::create(['name' => 'TEKNOLOGI & MANAJEMEN PERIKANAN BUDIDAYA', 'faculty_id' => 3]);
-        Department::create(['name' => 'MANAJEMEN SUMBERDAYA PERAIRAN', 'faculty_id' => 3]);
-        Department::create(['name' => 'TEKNOLOGI HASIL PERAIRAN', 'faculty_id' => 3]);
-        Department::create(['name' => 'TEKNOLOGI & MANAJEMEN PERIKANAN TANGKAP', 'faculty_id' => 3]);
-        Department::create(['name' => 'ILMU DAN TEKNOLOGI KELAUTAN', 'faculty_id' => 3]);
+        Department::create(['name' => 'Teknologi & Manajemen Perikanan Budidaya', 'faculty_id' => 3]);
+        Department::create(['name' => 'Manajemen Sumberdaya Perairan', 'faculty_id' => 3]);
+        Department::create(['name' => 'Teknologi Hasil Perairan', 'faculty_id' => 3]);
+        Department::create(['name' => 'Teknologi & Manajemen Perikanan Tangkap', 'faculty_id' => 3]);
+        Department::create(['name' => 'Ilmu Dan Teknologi Kelautan', 'faculty_id' => 3]);
 
-        Department::create(['name' => 'TEKNOLOGI PRODUKSI TERNAK', 'faculty_id' => 4]);
-        Department::create(['name' => 'NUTRISI DAN TEKNOLOGI PAKAN', 'faculty_id' => 4]);
-        Department::create(['name' => 'TEKNOLOGI HASIL TERNAK', 'faculty_id' => 4]);
+        Department::create(['name' => 'Teknologi Produksi Ternak', 'faculty_id' => 4]);
+        Department::create(['name' => 'Nutrisi Dan Teknologi Pakan', 'faculty_id' => 4]);
+        Department::create(['name' => 'Teknologi Hasil Ternak', 'faculty_id' => 4]);
 
-        Department::create(['name' => 'MANAJEMEN HUTAN', 'faculty_id' => 5]);
-        Department::create(['name' => 'TEKNOLOGI HASIL HUTAN', 'faculty_id' => 5]);
-        Department::create(['name' => 'KONSERVASI SUMBERDAYA HUTAN & EKOWISATA', 'faculty_id' => 5]);
-        Department::create(['name' => 'SILVIKULTUR', 'faculty_id' => 5]);
+        Department::create(['name' => 'Manajemen Hutan', 'faculty_id' => 5]);
+        Department::create(['name' => 'Teknologi Hasil Hutan', 'faculty_id' => 5]);
+        Department::create(['name' => 'Konservasi Sumberdaya Hutan & Ekowisata', 'faculty_id' => 5]);
+        Department::create(['name' => 'Silvikultur', 'faculty_id' => 5]);
 
-        Department::create(['name' => 'TEKNIK PERTANIAN DAN BIOSISTEM', 'faculty_id' => 6]);
-        Department::create(['name' => 'TEKNOLOGI PANGAN', 'faculty_id' => 6]);
-        Department::create(['name' => 'TEKNIK INDUSTRI PERTANIAN', 'faculty_id' => 6]);
-        Department::create(['name' => 'TEKNIK SIPIL DAN LINGKUNGAN', 'faculty_id' => 6]);
+        Department::create(['name' => 'Teknik Pertanian Dan Biosistem', 'faculty_id' => 6]);
+        Department::create(['name' => 'Teknologi Pangan', 'faculty_id' => 6]);
+        Department::create(['name' => 'Teknik Industri Pertanian', 'faculty_id' => 6]);
+        Department::create(['name' => 'Teknik Sipil Dan Lingkungan', 'faculty_id' => 6]);
 
-        Department::create(['name' => 'STATISTIKA DAN SAINS DATA', 'faculty_id' => 7]);
-        Department::create(['name' => 'METEOROLOGI TERAPAN', 'faculty_id' => 7]);
-        Department::create(['name' => 'BIOLOGI', 'faculty_id' => 7]);
-        Department::create(['name' => 'KIMIA', 'faculty_id' => 7]);
-        Department::create(['name' => 'MATEMATIKA', 'faculty_id' => 7]);
-        Department::create(['name' => 'ILMU KOMPUTER', 'faculty_id' => 7]);
-        Department::create(['name' => 'FISIKA', 'faculty_id' => 7]);
-        Department::create(['name' => 'BIOKIMIA', 'faculty_id' => 7]);
-        Department::create(['name' => 'AKTUARIA', 'faculty_id' => 7]);
+        Department::create(['name' => 'Statistika Dan Sains Data', 'faculty_id' => 7]);
+        Department::create(['name' => 'Meteorologi Terapan', 'faculty_id' => 7]);
+        Department::create(['name' => 'Biologi', 'faculty_id' => 7]);
+        Department::create(['name' => 'Kimia', 'faculty_id' => 7]);
+        Department::create(['name' => 'Matematika', 'faculty_id' => 7]);
+        Department::create(['name' => 'Ilmu Komputer', 'faculty_id' => 7]);
+        Department::create(['name' => 'Fisika', 'faculty_id' => 7]);
+        Department::create(['name' => 'Biokimia', 'faculty_id' => 7]);
+        Department::create(['name' => 'Aktuaria', 'faculty_id' => 7]);
 
-        Department::create(['name' => 'EKONOMI PEMBANGUNAN', 'faculty_id' => 8]);
-        Department::create(['name' => 'MANAJEMEN', 'faculty_id' => 8]);
-        Department::create(['name' => 'AGRIBISNIS', 'faculty_id' => 8]);
-        Department::create(['name' => 'EKONOMI SUMBERDAYA DAN LINGKUNGAN', 'faculty_id' => 8]);
-        Department::create(['name' => 'ILMU EKONOMI SYARIAH', 'faculty_id' => 8]);
+        Department::create(['name' => 'Ekonomi Pembangunan', 'faculty_id' => 8]);
+        Department::create(['name' => 'Manajemen', 'faculty_id' => 8]);
+        Department::create(['name' => 'Agribisnis', 'faculty_id' => 8]);
+        Department::create(['name' => 'Ekonomi Sumberdaya Dan Lingkungan', 'faculty_id' => 8]);
+        Department::create(['name' => 'Ilmu Ekonomi Syariah', 'faculty_id' => 8]);
 
-        Department::create(['name' => 'ILMU GIZI', 'faculty_id' => 9]);
-        Department::create(['name' => 'ILMU KELUARGA DAN KONSUMEN', 'faculty_id' => 9]);
-        Department::create(['name' => 'KOMUNIKASI DAN PENGEMBANGAN MASYARAKAT', 'faculty_id' => 9]);
+        Department::create(['name' => 'Ilmu Gizi', 'faculty_id' => 9]);
+        Department::create(['name' => 'Ilmu Keluarga Dan Konsumen', 'faculty_id' => 9]);
+        Department::create(['name' => 'Komunikasi Dan Pengembangan Masyarakat', 'faculty_id' => 9]);
 
-        Department::create(['name' => 'BISNIS', 'faculty_id' => 10]);
+        Department::create(['name' => 'Bisnis', 'faculty_id' => 10]);
 
-        Department::create(['name' => 'KOMUNIKASI', 'faculty_id' => 11]);
-        Department::create(['name' => 'EKOWISATA', 'faculty_id' => 11]);
-        Department::create(['name' => 'MANAJEMEN INFORMATIKA', 'faculty_id' => 11]);
-        Department::create(['name' => 'TEKNIK KOMPUTER', 'faculty_id' => 11]);
-        Department::create(['name' => 'SUPERVISOR JAMINAN MUTU PANGAN', 'faculty_id' => 11]);
-        Department::create(['name' => 'MANAJEMEN INDUSTRI JASA MAKANAN DAN GIZI', 'faculty_id' => 11]);
-        Department::create(['name' => 'TEKNOLOGI INDUSTRI BENIH', 'faculty_id' => 11]);
-        Department::create(['name' => 'TEKNOLOGI PRODUKSI DAN MANAJEMEN PERIKANAN BUDIDAYA', 'faculty_id' => 11]);
-        Department::create(['name' => 'TEKNOLOGI DAN MANAJEMEN TERNAK', 'faculty_id' => 11]);
-        Department::create(['name' => 'MANAJEMEN AGRIBISNIS', 'faculty_id' => 11]);
-        Department::create(['name' => 'MANAJEMEN INDUSTRI', 'faculty_id' => 11]);
-        Department::create(['name' => 'ANALISIS KIMIA', 'faculty_id' => 11]);
-        Department::create(['name' => 'TEKNIK DAN MANAJEMEN LINGKUNGAN', 'faculty_id' => 11]);
-        Department::create(['name' => 'AKUNTANSI', 'faculty_id' => 11]);
-        Department::create(['name' => 'PARAMEDIK VETERINER', 'faculty_id' => 11]);
-        Department::create(['name' => 'TEKNOLOGI PRODUKSI DAN MANAJEMEN PERKEBUNAN', 'faculty_id' => 11]);
-        Department::create(['name' => 'TEKNOLOGI PRODUKSI DAN PENGEMBANGAN MASYARAKAT PERTANIAN', 'faculty_id' => 11]);
+        Department::create(['name' => 'Komunikasi', 'faculty_id' => 11]);
+        Department::create(['name' => 'Ekowisata', 'faculty_id' => 11]);
+        Department::create(['name' => 'Manajemen Informatika', 'faculty_id' => 11]);
+        Department::create(['name' => 'Teknik Komputer', 'faculty_id' => 11]);
+        Department::create(['name' => 'Supervisor Jaminan Mutu Pangan', 'faculty_id' => 11]);
+        Department::create(['name' => 'Manajemen Industri Jasa Makanan Dan Gizi', 'faculty_id' => 11]);
+        Department::create(['name' => 'Teknologi Industri Benih', 'faculty_id' => 11]);
+        Department::create(['name' => 'Teknologi Produksi Dan Manajemen Perikanan Budidaya', 'faculty_id' => 11]);
+        Department::create(['name' => 'Teknologi Dan Manajemen Ternak', 'faculty_id' => 11]);
+        Department::create(['name' => 'Manajemen Agribisnis', 'faculty_id' => 11]);
+        Department::create(['name' => 'Manajemen Industri', 'faculty_id' => 11]);
+        Department::create(['name' => 'Analisis Kimia', 'faculty_id' => 11]);
+        Department::create(['name' => 'Teknik Dan Manajemen Lingkungan', 'faculty_id' => 11]);
+        Department::create(['name' => 'Akuntansi', 'faculty_id' => 11]);
+        Department::create(['name' => 'Paramedik Veteriner', 'faculty_id' => 11]);
+        Department::create(['name' => 'Teknologi Produksi Dan Manajemen Perkebunan', 'faculty_id' => 11]);
+        Department::create(['name' => 'Teknologi Produksi Dan Pengembangan Masyarakat Pertanian', 'faculty_id' => 11]);
     }
 }
