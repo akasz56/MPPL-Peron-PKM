d375d069aac3a1130a4f5279093d64cddb1e8dab
dahlah <hamdanalam11@gmail.com>
2022-10-27 22:20:03 +0700

    register middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'creator' => \App\Http\Middleware\CreatorMiddleware::class,
+        'developer' => \App\Http\Middleware\DeveloperMiddleware::class,
     ];
 }
