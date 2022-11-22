643ff0dc44dde8b42aeaddde1272e13b01d90c6f
Y0sar <ganshoyosar@gmail.com>
2022-11-22 13:52:37 +0700

    tambah search result view

---
+++ resources/views/search-result.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <x-slot name="header">
+        Hasil Pencarian "{{ $searchQuery }}"
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="p-6 bg-white border-gray-200 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="flex flex-wrap bg-white border-gray-200">
+                    @foreach ($data as $vacancy)
+                        <x-cards.vacancy :vacancy="$vacancy" />
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
